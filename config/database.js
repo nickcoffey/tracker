a5545df2ee307bdf55a074d39ab5aeaae5814f10
Nick <njc3n3@gmail.com>
2019-10-01 13:59:04 -0500

    Added mlab connection

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports.mlab = {
+    host: 'ds229088.mlab.com',
+    user: 'root1',
+    password: process.env.MLAB_PW,
+    port: '29088',
+    database: 'tracker'
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 var port = 3000
+const config = require('./config/database')
 
 // CORS Setup
 const cors = require('cors')
@@ -16,6 +17,21 @@ app.use(bodyParser.json())
 const log4js = require('log4js')
 const logger = log4js.getLogger()
 logger.level = 'TRACE'
+
+// DB setup
+const mlab = config.mlab
+const mongoose = require('mongoose')
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+const connection = module.exports = mongoose.connect(`mongodb://${mlab.user}:${mlab.password}@${mlab.host}:${mlab.port}/${mlab.database}`, (err, db) => {
+    if(err){
+        logger.error('DB connection failed', err)
+    } else {
+        logger.info('DB connection successful')
+    }
+})
 
 // Start app
 app.listen(port, () => {logger.info(`App listen is listening on port ${port}`)})
